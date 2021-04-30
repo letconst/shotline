74ac6f11abfde96f25a3568c9f2ca228790ea31d
Tekito10029 <tekito10029582@gmail.com>
2021-04-30 18:22:04 +0900

    プレイヤー移動の追加

---
+++ Assets/Users/Tokui/Resources/Models/UnityChan/Scripts/ItemHit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemHit : MonoBehaviour
+{
+
+
+    //当たった時のエフェクトを保存しておく変数
+    public GameObject effect;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // タグチェック
+        if(collision.gameObject.tag == "Player")
+        {
+            //エフェクト作成
+            Instantiate(effect, this.transform.position, Quaternion.identity);
+            //アイテムは削除
+            Destroy(this.gameObject);
+        }
+    }
+}
