349faf4969a10c4e2956b09f22c48ccd254b3e3f
letconst <2000takeru93@gmail.com>
2021-05-20 20:07:28 +0900

    [Add] アイテムの位置データ保持用のクラス作成（Bolt用）

---
+++ Assets/Users/Endo/Scripts/Item/Base/ItemPositionData.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// アイテムの生成位置情報（Bolt用）
+/// </summary>
+public class ItemPositionData : MonoBehaviour
+{
+    /// <summary>
+    /// 生成位置。
+    /// </summary>
+    public Vector3 Position => transform.position;
+
+    /// <summary>
+    /// この場所にアイテムが生成されているか。
+    /// </summary>
+    public bool isSpawned;
+
+    /// <summary>
+    /// アイテムが生成されてからの経過時間。
+    /// この変数に秒数を代入すると、自動的に時間が減少していき、ゼロ以下になるとゼロに固定されます。
+    /// </summary>
+    public float elapsedTimeFromSpawned;
+
+    private void Update()
+    {
+        if (elapsedTimeFromSpawned > 0)
+        {
+            elapsedTimeFromSpawned -= Time.deltaTime;
+        }
+        else
+        {
+            elapsedTimeFromSpawned = 0;
+        }
+    }
+}
