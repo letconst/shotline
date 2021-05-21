03fc0b6fbbbf06ad4d108d9e3aa44a85d6145bf1
letconst <2000takeru93@gmail.com>
2021-05-21 13:17:11 +0900

    [Update] アイテム基底クラス改良

--- Assets/Users/Endo/Scripts/Item/Apple.cs
+++ Assets/Users/Endo/Scripts/Item/Apple.cs
@@ -1,22 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class Apple : ActiveItem
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    protected override void Function()
+    protected override void UpdateFunction()
     {
         throw new System.NotImplementedException();
     }
--- Assets/Users/Endo/Scripts/Item/Base/ItemBase.cs
+++ Assets/Users/Endo/Scripts/Item/Base/ItemBase.cs
@@ -7,8 +7,41 @@ public abstract class ItemBase : MonoBehaviour
 
     public ItemData Data => data;
 
+    private bool _isEnabled;
+    private bool _isInitialized;
+
+    private void Update()
+    {
+        if (!_isEnabled) return;
+
+        if (!_isInitialized)
+        {
+            Init();
+            _isInitialized = true;
+        }
+
+        UpdateFunction();
+    }
+
     /// <summary>
-    /// アイテムを取得・使用した際の動作
+    /// アイテムを使用開始した際の動作
     /// </summary>
-    protected abstract void Function();
+    public virtual void Init()
+    {
+        _isEnabled = true;
+    }
+
+    /// <summary>
+    /// 毎フレームのアイテムの動作
+    /// </summary>
+    protected abstract void UpdateFunction();
+
+    /// <summary>
+    /// アイテムの使用が終了した際の動作
+    /// </summary>
+    protected virtual void Terminate()
+    {
+        _isEnabled     = false;
+        _isInitialized = false;
+    }
 }
