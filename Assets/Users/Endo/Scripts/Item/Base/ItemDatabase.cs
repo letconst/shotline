ced74a4add39852bbe40a45285116ee6f914e58d
letconst <2000takeru93@gmail.com>
2021-05-30 22:42:15 +0900

    [Update] メインシーンにアイテム自動生成機能取り込み

--- Assets/Users/Endo/Scripts/Item/Base/ItemDatabase.cs
+++ Assets/Users/Endo/Scripts/Item/Base/ItemDatabase.cs
@@ -19,7 +19,6 @@ public class ItemDatabase : MonoBehaviour
     /// <returns>アイテムデータ</returns>
     public static ItemData GetRandomItem()
     {
-        Debug.Log(ItemList);
         return ItemList[Random.Range(0, ItemList.Count)];
     }
 
