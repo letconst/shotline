e8e594a4c5c86719c0bc524e67a4909dfe73e284
letconst <2000takeru93@gmail.com>
2021-06-02 13:28:07 +0900

    [Update] プレイヤーへのカメラ追従を一旦スクリプト側で実装

--- Assets/Users/Endo/Scripts/Camera/CameraController.cs
+++ Assets/Users/Endo/Scripts/Camera/CameraController.cs
@@ -7,12 +7,23 @@ namespace Endo
         [SerializeField, Header("メインカメラと位置を同期させるカメラ")]
         private Camera[] followCameras;
 
+        private Transform _playerTrf;
+        private Vector3   _offsetPos;
+
         private void Start()
         {
             foreach (Camera cam in followCameras)
             {
                 cam.transform.rotation = transform.rotation;
             }
+
+            _playerTrf = GameObject.FindGameObjectWithTag("Player").transform;
+            _offsetPos = transform.position - _playerTrf.position;
+        }
+
+        private void Update()
+        {
+            transform.position = _playerTrf.position + _offsetPos;
         }
 
         private void LateUpdate()
