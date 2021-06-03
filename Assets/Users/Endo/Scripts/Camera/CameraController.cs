075154d29cfffa7c78eba580f370cfc3c3694a91
letconst <2000takeru93@gmail.com>
2021-06-04 01:56:46 +0900

    [Chore] プレイヤーへのカメラ追従をBolt仕様に差し戻し

--- Assets/Users/Endo/Scripts/Camera/CameraController.cs
+++ Assets/Users/Endo/Scripts/Camera/CameraController.cs
@@ -7,24 +7,16 @@ namespace Endo
         [SerializeField, Header("メインカメラと位置を同期させるカメラ")]
         private Camera[] followCameras;
 
-        private Transform _playerTrf;
-        private Vector3   _offsetPos;
-
         private void Start()
         {
             foreach (Camera cam in followCameras)
             {
                 cam.transform.rotation = transform.rotation;
             }
-
-            _playerTrf = GameObject.FindGameObjectWithTag("Player").transform;
-            _offsetPos = transform.position - _playerTrf.position;
         }
 
         private void Update()
         {
-            transform.position = _playerTrf.position + _offsetPos;
-            
             foreach (Camera cam in followCameras)
             {
                 cam.transform.position = transform.position;
