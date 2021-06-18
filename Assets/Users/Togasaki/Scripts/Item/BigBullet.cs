c87dafa85dc6b54268083843d2433f8522f5d0b3
tgsk <opnj0517@gmail.com>
2021-06-18 18:22:44 +0900

    アイテム残り回数処理途中

--- Assets/Users/Togasaki/Scripts/Item/BigBullet.cs
+++ Assets/Users/Togasaki/Scripts/Item/BigBullet.cs
@@ -1,24 +1,32 @@
 using UnityEngine;
 
-public class BigBullet : PassiveItem
+public class BigBullet : ActiveItem
 {
     //BigBulletが有効の時用のbool、使い果たすとfalse
     public static bool BBOn = false;
     public static bool BBOff = false;
 
+    public static bool OneBB = true;
 
+    public static bool ClickBB = false;
+
+
+    public GameObject stBtn;
 
     //最初に実行される
     protected override void Init()
     {
         base.Init();
 
-        Projectile.BBnum = 3;
+        Projectile.BBnum = 0;
+
+        stBtn = GameObject.Find("Shot");
 
         //BBOnをつける
         BBOff = false;
         BBOn = true;
-        Projectile.ScaleRatio = 1.5f;
+        OneBB = true;
+        ClickBB = false;
 
     }
 
@@ -27,11 +35,33 @@ public class BigBullet : PassiveItem
     {
         BBOff = false;
         BBOn = false;
-        Projectile.ScaleRatio = 1f;
 
         base.ClearItemIcon();
 
         base.Terminate();
+
+    }
+
+    protected override void OnClickButton()
+    {
+        
+        if (BBOn && Projectile.Line != null && Projectile.Line.enabled && OneBB)
+        {
+            ClickBB = true;
+
+            Projectile.ScaleRatio = 1.5f;
+            Projectile.BBnum++;
+
+            OneBB = false;
+            if (Projectile.BBnum == 3)
+            {
+                OneBB = true;
+                BBOff = true;
+
+            }
+
+            stBtn.GetComponent<Projectile>().Fire();
+        }
 
     }
 
