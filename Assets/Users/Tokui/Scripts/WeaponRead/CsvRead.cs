68982c3992ea9c50891549f4ca7eab3f961bd4d9
Tekito10029 <tekito10029582@gmail.com>
2021-07-16 18:14:19 +0900

    CSVファイルの読み込みの追加

---
+++ Assets/Users/Tokui/Scripts/WeaponRead/CsvRead.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CsvRead : MonoBehaviour
+{
+    TextAsset csvFile; // CSVファイル
+    List<string[]> csvDatas = new List<string[]>(); // CSVの中身を入れるリスト;
+
+    public void Start()
+    {
+        csvFile = Resources.Load("Data/WeaponData") as TextAsset; // Resouces下のCSV読み込み
+        StringReader reader = new StringReader(csvFile.text);
+
+        // , で分割しつつ一行ずつ読み込み
+        // リストに追加していく
+        reader.ReadLine(); // 1行読み捨てる。
+        while (reader.Peek() != -1) // reader.Peaekが-1になるまで
+        {
+            string line = reader.ReadLine(); // 一行ずつ読み込み
+            csvDatas.Add(line.Split(',')); // , 区切りでリストに追加
+        }
+
+        // csvDatas[行][列]を指定して値を自由に取り出せる
+        Debug.Log(csvDatas/*行*/[0]/*列*/[1]);
+    }
+}
---
+++ Assets/Users/Tokui/Scripts/WeaponRead/WeaponDadas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDadas
+{
+    public string WeaponName;
+
+    public string PrefabsName;
+
+    public float BulletSpeed;
+
+    public float GaugeMax;
+
+    public float GaugeRecovery;
+}
