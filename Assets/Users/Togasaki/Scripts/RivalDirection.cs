2695a4d1d129d243bb63be6ab4474b23b2976de7
tgsk <opnj0517@gmail.com>
2021-08-21 11:23:45 +0900

    敵方向仮完成

--- Assets/Users/Togasaki/Scripts/RivalDirection.cs
+++ Assets/Users/Togasaki/Scripts/RivalDirection.cs
@@ -6,6 +6,12 @@ public class RivalDirection : MonoBehaviour
     [SerializeField, Header("矢印イメージ")]
     private Image Arrow;
 
+    [SerializeField, Header("MAX位置X")]
+    private float maxPosX = 880;
+
+    [SerializeField, Header("MAX位置Y")]
+    private float maxPosY = 640;
+
     //プレイヤー格納変数
     GameObject PlayerCharacter;
 
@@ -14,6 +20,13 @@ public class RivalDirection : MonoBehaviour
 
     //イメージのrectTransform
     RectTransform arrowRT;
+
+    //rectTransform位置
+    float posX;
+    float posY;
+
+    //位置の割合
+    float ratio;
 
     void Start()
     {
@@ -33,10 +46,49 @@ public class RivalDirection : MonoBehaviour
         float radian = Mathf.Atan2(RivalCharacter.transform.position.z - PlayerCharacter.transform.position.z, RivalCharacter.transform.position.x - PlayerCharacter.transform.position.x);
         float deg = radian * (180 / 3.14f);
         arrowRT.rotation = Quaternion.Euler(0, 0, deg - 90);
-        
 
-        //これで矢印の角度が分かる
-        //Debug.Log(deg);
+        if (deg >= -45 && deg < 0)
+        {
+            ratio = Mathf.InverseLerp(-45, 0, deg);
+            arrowRT.localPosition = new Vector3(-maxPosX, Mathf.Lerp(maxPosY, 0, ratio), 0);
+        }
+        else if (deg >= 0 && deg < 45)
+        {
+            ratio = Mathf.InverseLerp(0, 45, deg);
+            arrowRT.localPosition = new Vector3(-maxPosX, Mathf.Lerp(0, -maxPosY, ratio), 0);
+        }
+        else if (deg >= 45 && deg < 90)
+        {
+            ratio = Mathf.InverseLerp(45, 90, deg);
+            arrowRT.localPosition = new Vector3(Mathf.Lerp(-maxPosX, 0, ratio), -maxPosY, 0);
+        }
+        else if (deg >= 90 && deg < 135)
+        {
+            ratio = Mathf.InverseLerp(90, 135, deg);
+            arrowRT.localPosition = new Vector3(Mathf.Lerp(0, maxPosX, ratio), -maxPosY, 0);
+        }
+        else if (deg >= 135 && deg < 180)
+        {
+            ratio = Mathf.InverseLerp(135, 180, deg);
+            arrowRT.localPosition = new Vector3(maxPosX, Mathf.Lerp(-maxPosY, 0, ratio), 0);
+        }
+        else if (deg >= -180 && deg < -135)
+        {
+            ratio = Mathf.InverseLerp(-180, -135, deg);
+            arrowRT.localPosition = new Vector3(maxPosX, Mathf.Lerp(0, maxPosY, ratio), 0);
+        }
+        else if (deg >= -135 && deg < -90)
+        {
+            ratio = Mathf.InverseLerp(-135, -90, deg);
+            arrowRT.localPosition = new Vector3(Mathf.Lerp(maxPosX, 0, ratio), maxPosY, 0);
+        }
+        else if (deg >= -90 && deg < -45)
+        {
+            ratio = Mathf.InverseLerp(-90, -45, deg);
+            arrowRT.localPosition = new Vector3(Mathf.Lerp(0, -maxPosX, ratio), maxPosY, 0);
+        }
+
     }
 
+    
 }
