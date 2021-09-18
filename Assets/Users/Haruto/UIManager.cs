037f137cd8281fd83514bad1275d3388dd8961a9
harutonara <haruto.nara.2000@gmail.com>
2021-09-19 03:43:33 +0900

    メニュー:マネージャー(未完成)

---
+++ Assets/Users/Haruto/UIManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BattleStart()
+    {
+        SceneManager.sceneLoaded += MenuToBattleExchange;
+
+        SceneManager.LoadScene("ForestScene"); // タスク１
+    }
+
+    private void MenuToBattleExchange(Scene a_scene, LoadSceneMode a_mode)
+    {
+        var l_object = GameObject.Find("Player"); // タスク２
+        var l_script = l_object.GetComponent<PlayerController>(); // タスク２
+
+        SceneManager.sceneLoaded -= MenuToBattleExchange;
+    }
+}
