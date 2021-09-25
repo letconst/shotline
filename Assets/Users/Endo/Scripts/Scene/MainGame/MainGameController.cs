b8e38661a09ecb4c586523ac617b1ab5bb79d293
letconst <2000takeru93@gmail.com>
2021-09-26 02:54:01 +0900

    [Fix] 相手切断時にタイトルへ戻った際、自身がルームに残り続けていることになるのを修正

--- Assets/Users/Endo/Scripts/Scene/MainGame/MainGameController.cs
+++ Assets/Users/Endo/Scripts/Scene/MainGame/MainGameController.cs
@@ -284,6 +284,7 @@ public class MainGameController : SingletonMonoBehaviour<MainGameController>, IM
                 SystemUIManager.OpenAlertWindow("Information", "対戦相手が切断しました。タイトルに戻ります。",
                                                 () =>
                                                 {
+                                                    NetworkManager.Disconnect();
                                                     SystemSceneManager.LoadNextScene("Title", SceneTransition.Fade);
                                                 });
 
