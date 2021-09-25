b0d9213609725ce7e9f3ae9570ac6888aa29ca46
letconst <2000takeru93@gmail.com>
2021-09-25 16:03:58 +0900

    [Update] シールドモデル差し替え・アニメーションするように

--- Assets/Users/Togasaki/Scripts/Item/Shield.cs
+++ Assets/Users/Togasaki/Scripts/Item/Shield.cs
@@ -42,7 +42,7 @@ public class Shield : ActiveItem
 
         if (NetworkManager.IsConnected)
         {
-            NetworkManager.Instantiate("Prefabs/Shield", pos + vec * 1, Player.transform.rotation);
+            NetworkManager.Instantiate("Prefabs/Models/Shield", pos + vec * 1, Player.transform.rotation);
         }
         else
         {
